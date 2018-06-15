c0f7cc37bb2e2a6a8e80db851ef4a9fceacc09be
aslancll <celalaslan@outlook.com>
2018-06-15 14:46:49 -0700

    Initial Commit

---
+++ Time Tracer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Time Tracer
+//
+//  Created by Celal Aslan on 2018-06-15.
+//  Copyright © 2018 Celal Aslan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
